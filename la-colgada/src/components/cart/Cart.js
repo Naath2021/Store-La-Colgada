6828cbf5d0393f1b9503bc3271551ff26af562d8
Nathaly Learmonth <learmonth.nathaly@gmail.com>
2022-10-04 22:23:57 -0300

    fix/ removed commented cartBody on Cart

--- la-colgada/src/components/cart/Cart.js
+++ la-colgada/src/components/cart/Cart.js
@@ -30,32 +30,6 @@ function Cart() {
           <Offcanvas.Title>¡tu carrito!</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-
-          {/* {cart.length === 0
-            ? <>
-              <h6 className='texts empty-cart'>estoy vacío, ¡lléname de productos!</h6>
-              <Link to="products" className='link-router'>ver productos</Link>
-            </>
-            : cart.map((item) => {
-              return <div key={item.product.id} className="cart-body-container">
-                <div className='item-container'>
-                  <div className="cart-img-container">
-                    <img src={baseUrl + item.product.image1} alt={item.product.name} className="item-img b-radius-5" />
-                  </div>
-                  <div className='cart-item-info-container'>
-                    <button onClick={() => deleteProduct(item)} className="delete-product-btn"><Popup content='Eliminar producto' trigger={<FontAwesomeIcon icon={faXmark} />} /></button>
-                    <h2 className='texts cart-item-name'>{item.product.name}</h2>
-                    <h6 className="texts cart-item-price">{item.qty} x ${item.productTotalPrice}</h6>
-                  </div>
-                </div>
-              </div>
-            })
-          }
-          <h5 className='texts cart-total-price'> total: ${getTotalCartPrice()}</h5>
-          <h6 onClick={() => clearCart()} className="clear-cart">vaciar carrito</h6>
-          {btnOrderCheckout} */}
-
-
           <CartBody cart={cart} baseUrl={baseUrl} getTotalCartPrice={getTotalCartPrice} deleteProduct={deleteProduct} clearCart={clearCart} />
         </Offcanvas.Body>
       </Offcanvas>
